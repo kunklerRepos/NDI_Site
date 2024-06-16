a9b3f6bb43ccd23db9eabe94f98cdac6a5e1e76f
Skunkler <42595638+Skunkler@users.noreply.github.com>
2024-06-16 14:42:25 -0400

    updates

--- js/Services.js
+++ js/Services.js
@@ -11,13 +11,13 @@ const Services = (isPortrait) =>
             <img src="assets/image001 (1).jpg" class="rounded-circle img-fluid"/>
         </div>
         <div class="col-md-6">
-                <h4>Products</h4>
+                <h4 class="text-regular">Products</h4>
             <p class="text-regular">
                 NDI specializes in hydro-mechanical actuators, fluidic control valves, rotor brakes, and directional control values. Our product portfolio that includes proprietary landing gear system, actuator, valve, damper, and rotor brake designs.
             </p>
         </div>
         <div class="col-md-6">
-            <h4>Values</h4>
+            <h4 class="text-regular">Values</h4>
             <p class="text-regular">
                 To preserve core skills and remain flexible, NDI has developed a low overhead organizational structure centered on the key functions of engineering, quality assurance, production control, production machining and assembly and test.
             </p>
@@ -30,8 +30,8 @@ const Services = (isPortrait) =>
             <img src="assets/image001.jpg" class="rounded-circle img-fluid">
         </div>
         <div class="col-md-6">
-            <h4>Strategy</h4>
-            <p>NDI utilized a comfortable mix of in-house machining and subcontracted operations of various components. This approach yields a very efficient business model with the ability to quickly expand or contract based on the business climate of the industry. This model has proven to be very successful with NDI weathering numerous recessions as well as the recent unprecedented pandemic.</p>
+            <h4 class="text-regular">Strategy</h4>
+            <p class="text-regular">NDI utilized a comfortable mix of in-house machining and subcontracted operations of various components. This approach yields a very efficient business model with the ability to quickly expand or contract based on the business climate of the industry. This model has proven to be very successful with NDI weathering numerous recessions as well as the recent unprecedented pandemic.</p>
         </div>
     </div>
         `;
