7fa1743cfb295cc827266116321bd19f52cda33b
Skunkler <42595638+Skunkler@users.noreply.github.com>
2024-06-22 22:51:25 -0400

    contact form update

--- js/BusinessModel.js
+++ js/BusinessModel.js
@@ -6,8 +6,8 @@ const BusinessModel = () =>
     let section = document.getElementById("businessModel");
 
     section.innerHTML = ` <div style="display: flex; justify-content: center;">
-                        <img src="assets/managementPrinciples.png" class="img-fluid rounded float-start" style="width: 100%; height: auto;"/>
-                    </div>
+                            <img src="assets/managementPrinciples.png" class="img-fluid rounded float-start" style="width: 50%; height: auto;"/>
+                          </div>
                     <div style="text-align: center; margin-top: 2%;">
                         <p class="text-regular form-horizontal">
                             NDI management recognizes that continual product quality and customer satisfaction can only be sustained by effective operation of the various related business processes within an organization. NDI has adopted a process approach to manage the various functions within the organization by establishing and implementing a Business and Quality Management System as Illustrated above.
--- js/contact.js
+++ js/contact.js
@@ -4,28 +4,28 @@ const ContactForm = () =>
     let contactSection = document.getElementById("contact");
 
     contactSection.innerHTML = `<section class="formcarry-container">
-    <form action="https://formcarry.com/s/Fzv_zezSXYq" method="POST" enctype="multipart/form-data">
-      
-      <div class="formcarry-block">
-        <label class="form-label text-regular" for="fc-generated-1-name">Full Name</label>
-        <input type="text" name="name" id="fc-generated-1-name" placeholder="Your first and last name" />
-      </div>
-        
-      <div class="formcarry-block">
-        <label class="form-label text-regular" for="fc-generated-1-email">Your Email Address</label>
-        <input type="email" name="email" id="fc-generated-1-email" placeholder="john@doe.com" />
-      </div>
-        
-      <div class="formcarry-block">
-        <label class="form-label text-regular" for="fc-generated-1-message">Your message</label>
-        <textarea name="message" name="message" id="fc-generated-1-message" placeholder="Enter your message..."></textarea>
-      </div>
-        
-      <div class="formcarry-block">  
-        <button type="submit" class="btn btn-primary btn-block mb-4" style="width: 100%; border-color: #401C90; background-color: #401C90;">Send</button>
-      </div>
+  <form action="https://formcarry.com/s/CVOTjwYv8iC" method="POST" enctype="multipart/form-data">
     
-    </form>
+    <div class="formcarry-block">
+      <label class="form-label text-regular" for="fc-generated-1-name">Full Name</label>
+      <input type="text" name="name" id="fc-generated-1-name" placeholder="Your first and last name" />
+    </div>
+  	
+    <div class="formcarry-block">
+      <label class="form-label text-regular" for="fc-generated-1-email">Your Email Address</label>
+      <input type="email" name="email" id="fc-generated-1-email" placeholder="john@doe.com" />
+    </div>
+  	
+    <div class="formcarry-block">
+      <label class="form-label text-regular" for="fc-generated-1-message">Your message</label>
+      <textarea name="message" name="message" id="fc-generated-1-message" placeholder="Enter your message..."></textarea>
+    </div>
+  	
+    <div class="formcarry-block">  
+      <button class="btn btn-primary btn-block mb-4" style="width: 100%; border-color: #401C90; background-color: #401C90;" type="submit">Send</button>
+    </div>
+  
+  </form>
     <div style="justify-content: center; display: grid; margin-top: 15%;">
         <h5 class="text-regular">Hours of Operation (PST)</h5>
         <a href="tel:3602533656" style="text-align: center">(360) 253-3656</a> 
