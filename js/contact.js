5474438f44d62f27e91511c71c44cf7db845f433
Skunkler <42595638+Skunkler@users.noreply.github.com>
2024-06-29 12:00:49 -0400

    management principles

--- js/contact.js
+++ js/contact.js
@@ -45,63 +45,7 @@ const ContactForm = () =>
 
   </section>`;
 
-    /*contactSection.innerHTML = `
-    <div style="display: grid; text-align: center; justify-content: center;">
-        <h5 class="text-regular">Email Us</h5>
-    </div>
-    <form class="form-horizontal" id="formContact" >
-    <div class="form-outline mb-4">
-
-        <label class="form-label text-regular" for="NameInput">Name</label>
-        <input type="text" id="NameInput" class="form-control" required/>
-        <div class="valid-tooltip">
-            Looks good!
-        </div>
-    </div>
-
-    <!-- Email input -->
-    <div class="form-outline mb-4">
-        <label class="form-label text-regular" for="form4Example2">Email address</label>
-        <input type="email" id="emailContact" class="form-control text-regular" required/>
-    </div>
-
-    <!-- Message input -->
-    <div class="form-outline mb-4">
-        <label class="form-label text-regular" for="form4Example3">Message</label>
-        <textarea class="form-control text-regular" id="form4Example3" rows="4" required></textarea>
-    </div>
-    <!--div class="g-recaptcha" data-sitekey="6Ld5sCwpAAAAAGhSgWmtGT4fZ6m33nmzZSUXp3UJ"></div-->
-    <!-- Submit button -->
-    
-    <button class="btn btn-primary btn-block mb-4" style="width: 100%; border-color: #401C90; background-color: #401C90;" onclick="${emailSubmission}">Send</button>
-    </form>
-    <div style="justify-content: center; display: grid; margin-top: 15%;">
-        <h5 class="text-regular">Hours of Operation (PST)</h5>
-        <a href="tel:3602533656" style="text-align: center">(360) 253-3656</a> 
-        <br>
-        <table>
-            
-            <tr><th class="text-regular">Monday:</th><td class="text-regular">7:30 am - 4:00 pm</td></tr>
-            <tr><th class="text-regular">Tuesday:</th><td class="text-regular">7:30 am - 4:00 pm</td></tr>
-            <tr><th class="text-regular">Wednesday:</th><td class="text-regular">7:30 am - 4:00 pm</td></tr>
-            <tr><th class="text-regular">Thursday:</th><td class="text-regular">7:30 am - 4:00 pm</td></tr>
-            <tr><th class="text-regular">Friday:</th><td class="text-regular">7:30 am - 4:00 pm</td></tr>
-            <tr><th class="text-regular">Saturday:</th><td class="text-regular">Closed</td></tr>
-            <tr><th class="text-regular">Sunday:</th><td class="text-regular">Closed</td></tr>
-        </table>
-
-    </div>
-
-
-    `;
-
-    const emailForm = document.getElementById("formContact");
-
-    emailForm.addEventListener("submit", emailSubmission);
-    document.getElementById('emailContact').onchange = emailValidation;*/
-
-
-
+ 
 };
 
 
